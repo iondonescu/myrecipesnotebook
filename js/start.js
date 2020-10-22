88314ed4e7bb562c2845f512fa55bb6a6fc91379
iondonescu <iondonescu@yahoo.com>
2020-10-22 18:26:56 +0300

    Comments in html

--- js/start.js
+++ js/start.js
@@ -2,31 +2,32 @@
 // for(let i=0;i<=3;i++){
 //     document.write("<button class='submit_button'> Acesta este un buton</button>")
 // }
-let picture = document.getElementsByClassName("food-images");
-picture[0].onmouseover = function(){
-    let right_button = document.getElementsByClassName("right-button");
-    right_button[0].style.visibility = "visible";
-    let left_button = document.getElementsByClassName("left-button");
-    left_button[0].style.visibility = "visible";
-
-    right_button[0].addEventListener("click", function(){
-        let img = document.getElementsByClassName("food-image");
-        img[0].classList.remove("food-image-display-block");
-        img[0].classList.add("food-image-display-none");
-        img[1].classList.remove("food-image-display-none");
-        img[1].classList.add("food-image-display-block");
-        // let img = document.createElement("img");
-        // img.src = "./images/friptura_de_porc_2.jpg";
-        // let images = document.getElementsByClassName("food-images");
-        // images[0].appendChild(img);
-    })
-}
-
-picture[0].onmouseout = function(){
-    let right_button = document.getElementsByClassName("right-button");
-    right_button[0].style.visibility = "hidden";
-    let left_button = document.getElementsByClassName("left-button");
-    left_button[0].style.visibility = "hidden";
-}
+// let picture = document.getElementsByClassName("food-images");
+// picture[0].onmouseover = function(){
+//     let right_button = document.getElementsByClassName("right-button");
+//     right_button[0].style.visibility = "visible";
+//     let left_button = document.getElementsByClassName("left-button");
+//     left_button[0].style.visibility = "visible";
+//
+//     right_button[0].addEventListener("click", function(){
+//         let img = document.getElementsByClassName("food-image");
+//         img[0].classList.remove("food-image-display-block");
+//         img[0].classList.add("food-image-display-none");
+//         img[1].classList.remove("food-image-display-none");
+//         img[1].classList.add("food-image-display-block");
+//         // let img = document.createElement("img");
+//         // img.src = "./images/friptura_de_porc_2.jpg";
+//         // let images = document.getElementsByClassName("food-images");
+//         // images[0].appendChild(img);
+//     })
+// }
+//
+// picture[0].onmouseout = function(){
+//     let right_button = document.getElementsByClassName("right-button");
+//     right_button[0].style.visibility = "hidden";
+//     let left_button = document.getElementsByClassName("left-button");
+//     left_button[0].style.visibility = "hidden";
+// }
 // let list = $('.item');
 // list.css('color','red');
+console.log($('.item').html());
